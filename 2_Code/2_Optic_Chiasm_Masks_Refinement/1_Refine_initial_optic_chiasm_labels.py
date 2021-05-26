d2642f73d3cf57431e26ac3fa905827d6635cebb
rjpuzniak <rjpuzniak@gmail.com>
2021-05-26 12:36:48 +0200

    Initial version of mask refinement

--- 2_Code/2_Optic_Chiasm_Masks_Refinement/1_Refine_initial_optic_chiasm_labels.py
+++ 2_Code/2_Optic_Chiasm_Masks_Refinement/1_Refine_initial_optic_chiasm_labels.py
@@ -2,7 +2,7 @@ from functions import bounding_box, create_groundtruth
 
 import os
 
-groups=['1_HCP','2_CHIASM']
+groups=['HCP','CHIASM']
 
 for group in groups:
 	for subject in os.listdir('../../1_Data/1_T1w_Images_and_Labels/1_T1w_Images/'+group):
